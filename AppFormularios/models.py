c3ad452b1a6e0c7cdc0867d63d568afffe6f26bf
Marcelo Ambrosio <marceambrosio.ok@gmail.com>
2023-12-27 10:27:35 -0300

    pdf completo

--- AppFormularios/models.py
+++ AppFormularios/models.py
@@ -9,10 +9,9 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
-from reportlab.platypus import Image
 from reportlab.lib.pagesizes import A4
-from reportlab.lib.units import inch
 from PIL import Image as PilImage
+from PyPDF2 import PdfReader, PdfWriter
 import io
 
 # Create your models here.
@@ -77,6 +76,10 @@ class ReempadComercioFisica(models.Model):
         # Crea el archivo PDF, usando el objeto como su "archivo"
         p = canvas.Canvas(buffer)
 
+        # Establecer el título del PDF
+        titulo = "Reempadronamiento-" + self.nombreFantasia + "-" + self.cuit
+        p.setTitle(titulo)
+
         # --- Dibuja la imagen de la portada ---
         # Carga la imagen de la portada
         portada = PilImage.open('img/formulario_encabezado.png')
@@ -108,35 +111,178 @@ class ReempadComercioFisica(models.Model):
         # Dibuja "Nombre de fantasía:" en negrita y luego el valor en texto normal
         label = "Nombre de fantasía: "
         p.setFont('Helvetica-Bold', 12)
-        p.drawString(50, 650, label)
+        p.drawString(50, 640, label)
         p.setFont('Helvetica', 12)
         length = p.stringWidth(label, "Helvetica-Bold", 12)
-        p.drawString(82 + length, 650, self.nombreFantasia)
+        p.drawString(73 + length, 640, self.nombreFantasia)
         
+        # Dibuja "CUIT:" en negrita y luego el valor en texto normal
         label = "CUIT: "
-        p.setFont('Helvetica-Bold', 12)
-        p.drawString(50, 630, label)
-        p.setFont('Helvetica', 12)
-        length = p.stringWidth(label, "Helvetica-Bold", 12)
-        p.drawString(100 + length, 630, self.cuit)
-        
-        label = "Insc. Ingresos Brutos: "
         p.setFont('Helvetica-Bold', 12)
         p.drawString(50, 610, label)
         p.setFont('Helvetica', 12)
         length = p.stringWidth(label, "Helvetica-Bold", 12)
-        p.drawString(61 + length, 610, self.ingresosBrutos)
-        
-        label = "Convenio Multilateral: "
+        p.drawString(70 + length, 610, self.cuit)
+
+        # Calcula la nueva posición x para "Insc. Ingresos Brutos:"
+        x = 70 + length + p.stringWidth(self.cuit, "Helvetica", 12) + 50
+
+        # Dibuja "Insc. Ingresos Brutos:" en negrita y luego el valor en texto normal
+        label = "Insc. Ingresos Brutos: "
         p.setFont('Helvetica-Bold', 12)
-        p.drawString(50, 590, label)
+        p.drawString(x, 610, label)
         p.setFont('Helvetica', 12)
         length = p.stringWidth(label, "Helvetica-Bold", 12)
-        p.drawString(61 + length, 590, self.convenioMultilateral)
+        p.drawString(x + length + 20, 610, self.ingresosBrutos)
+        
+        if self.convenioMultilateral is not None:
+            label = "Convenio Multilateral: "
+            p.setFont('Helvetica-Bold', 12)
+            p.drawString(50, 580, label)
+            p.setFont('Helvetica', 12)
+            length = p.stringWidth(label, "Helvetica-Bold", 12)
+            p.drawString(61 + length, 580, self.convenioMultilateral)
 
-        # Agrega más campos aquí...
+        # Dibuja una línea horizontal que divida la hoja
+        p.line(50, 550, A4[0] - 50, 550)
+        
+        label = "Domicilio Fiscal: "
+        p.setFont('Helvetica-Bold', 12)
+        p.drawString(50, 510, label)
+        p.setFont('Helvetica', 12)
+        length = p.stringWidth(label, "Helvetica-Bold", 12)
+        p.drawString(61 + length, 510, self.domicilioFiscal)
+        
+        # Dibuja "Localidad:" en negrita y luego el valor en texto normal
+        label = "Localidad: "
+        p.setFont('Helvetica-Bold', 12)
+        p.drawString(50, 480, label)
+        p.setFont('Helvetica', 12)
+        length = p.stringWidth(label, "Helvetica-Bold", 12)
+        p.drawString(50 + length, 480, self.localidad)
 
-        # Cierra el objeto PDF limpiamente.
+        # Calcula la nueva posición x para "Cod. Postal:"
+        x = 70 + length + p.stringWidth(self.localidad, "Helvetica", 12) + 50
+
+        # Dibuja "Cod. Postal:" en negrita y luego el valor en texto normal
+        label = "Cod. Postal: "
+        p.setFont('Helvetica-Bold', 12)
+        p.drawString(x, 480, label)
+        p.setFont('Helvetica', 12)
+        length = p.stringWidth(label, "Helvetica-Bold", 12)
+        p.drawString(x + length + 10, 480, str(self.codigoPostal))
+
+        # Calcula la nueva posición x para "Provincia:"
+        x = x + length + p.stringWidth(str(self.codigoPostal), "Helvetica", 12) + 50
+
+        # Dibuja "Provincia:" en negrita y luego el valor en texto normal
+        label = "Provincia: "
+        p.setFont('Helvetica-Bold', 12)
+        p.drawString(x, 480, label)
+        p.setFont('Helvetica', 12)
+        length = p.stringWidth(label, "Helvetica-Bold", 12)
+        p.drawString(x + length + 10, 480, self.provincia)
+        
+        label = "Domicilio Comercial: "
+        p.setFont('Helvetica-Bold', 12)
+        p.drawString(50, 450, label)
+        p.setFont('Helvetica', 12)
+        length = p.stringWidth(label, "Helvetica-Bold", 12)
+        p.drawString(61 + length, 450, self.domicilioComercial)
+        
+        label = "E-mail: "
+        p.setFont('Helvetica-Bold', 12)
+        p.drawString(50, 420, label)
+        p.setFont('Helvetica', 12)
+        length = p.stringWidth(label, "Helvetica-Bold", 12)
+        p.drawString(61 + length, 420, self.email)
+        
+        # Dibuja "Teléfono Comercial:" en negrita y luego el valor en texto normal
+        label = "Teléfono Comercial: "
+        p.setFont('Helvetica-Bold', 12)
+        p.drawString(50, 390, label)
+        p.setFont('Helvetica', 12)
+        length = p.stringWidth(label, "Helvetica-Bold", 12)
+        p.drawString(60 + length, 390, self.telefonoComercial)
+
+        # Calcula la nueva posición x para "Teléfono del Titular:"
+        x = 40 + length + p.stringWidth(self.telefonoComercial, "Helvetica", 12) + 50
+
+        # Dibuja "Teléfono del Titular:" en negrita y luego el valor en texto normal
+        label = "Teléfono del Titular: "
+        p.setFont('Helvetica-Bold', 12)
+        p.drawString(x, 390, label)
+        p.setFont('Helvetica', 12)
+        length = p.stringWidth(label, "Helvetica-Bold", 12)
+        p.drawString(x + length + 10, 390, self.telefonoTitular)
+        
+        label = "Superficie del local en m2: "
+        p.setFont('Helvetica-Bold', 12)
+        p.drawString(50, 360, label)
+        p.setFont('Helvetica', 12)
+        length = p.stringWidth(label, "Helvetica-Bold", 12)
+        p.drawString(61 + length, 360, str(self.superficieLocal))
+        
+        if self.superficieDeposito is not None:
+            label = "Superficie del depósito en m2: "
+            p.setFont('Helvetica-Bold', 12)
+            p.drawString(50, 360, label)
+            p.setFont('Helvetica', 12)
+            length = p.stringWidth(label, "Helvetica-Bold", 12)
+            p.drawString(61 + length, 360, str(self.superficieDeposito))
+
+        # Dibuja "Posee sucursal/es:" en negrita y luego el valor en texto normal
+        label = "Posee sucursal/es: "
+        p.setFont('Helvetica-Bold', 12)
+        p.drawString(50, 330, label)
+        p.setFont('Helvetica', 12)
+        length = p.stringWidth(label, "Helvetica-Bold", 12)
+        if self.sucursal:
+            p.drawString(55 + length, 330, "Sí")
+        else:
+            p.drawString(55 + length, 330, "No")
+
+        # Calcula la nueva posición x para "Domicilio Sucursal:"
+        x = 40 + length + p.stringWidth("Sí" if self.sucursal else "No", "Helvetica", 12) + 50
+
+        # Dibuja "Domicilio Sucursal:" en negrita y luego el valor en texto normal
+        if self.domicilioSucursal is not None:
+            label = "Domicilio Sucursal: "
+            p.setFont('Helvetica-Bold', 12)
+            p.drawString(x, 330, label)
+            p.setFont('Helvetica', 12)
+            length = p.stringWidth(label, "Helvetica-Bold", 12)
+            p.drawString(x + length + 20, 330, self.domicilioSucursal)
+        
+        # Dibuja una línea horizontal que divida la hoja
+        p.line(50, 300, A4[0] - 50, 300)
+
+        label = "Actividad Principal: "
+        p.setFont('Helvetica-Bold', 12)
+        p.drawString(50, 270, label)
+        p.setFont('Helvetica', 12)
+        length = p.stringWidth(label, "Helvetica-Bold", 12)
+        p.drawString(61 + length, 270, self.actividadPrincipal)
+        
+        if self.rubro2 is not None:
+            label = "Otros rubros de actividad: "
+            p.setFont('Helvetica-Bold', 12)
+            p.drawString(50, 240, label)
+            p.setFont('Helvetica', 12)
+            length = p.stringWidth(label, "Helvetica-Bold", 12)
+            p.drawString(61 + length, 240,"- " + self.rubro2)
+        
+        if self.rubro3 is not None:
+            p.setFont('Helvetica', 12)
+            length = p.stringWidth(label, "Helvetica-Bold", 12)
+            p.drawString(61 + length, 210,"- " + self.rubro3)
+        
+        if self.rubro4 is not None:
+            p.setFont('Helvetica', 12)
+            length = p.stringWidth(label, "Helvetica-Bold", 12)
+            p.drawString(61 + length, 180,"- " + self.rubro4)
+        
+    # Cierra el objeto PDF limpiamente.
         p.showPage()
         p.save()
 
@@ -144,8 +290,25 @@ class ReempadComercioFisica(models.Model):
         pdf = buffer.getvalue()
         buffer.close()
 
+        # Crea un objeto PdfWriter para escribir el PDF final
+        output = PdfWriter()
+
+        # Añade el PDF que acabas de generar
+        output.add_page(PdfReader(io.BytesIO(pdf)).pages[0])
+
+        # Añade el PDF existente
+        output.add_page(PdfReader(open(self.inscripcionAFIP.path, 'rb')).pages[0])
+
+        # Escribe el PDF final
+        with open('output.pdf', 'wb') as f:
+            output.write(f)
+
+        # Lee el contenido del nuevo archivo PDF
+        with open('output.pdf', 'rb') as f:
+            new_pdf = f.read()
+
         # Guarda el PDF en el campo 'pdf' del modelo.
-        self.pdf.save('output.pdf', ContentFile(pdf), save=False)
+        self.pdf.save('output.pdf', ContentFile(new_pdf), save=False)
 
         # Actualiza el estado
         estado = Estado.objects.get(nombre='Pendiente')
